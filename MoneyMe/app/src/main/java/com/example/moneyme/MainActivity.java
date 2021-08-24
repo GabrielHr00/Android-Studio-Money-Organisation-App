f3a8f873d9e4064cb9f8e8c1770e12a6524cef8e
gabrielh00 <a11914741@univie.ac.at>
2021-08-24 19:24:11 +0200

    Register screen and color changes

--- MoneyMe/app/src/main/java/com/example/moneyme/MainActivity.java
+++ MoneyMe/app/src/main/java/com/example/moneyme/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.moneyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterScreen.class));
+            }
+        });
     }
 }
