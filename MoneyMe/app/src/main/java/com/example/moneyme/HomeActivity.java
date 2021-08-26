900b90dab7757cc00bdb26a07066c864b1788dc5
gabrielh00 <a11914741@univie.ac.at>
2021-08-26 15:55:52 +0200

    App reorganisation

--- MoneyMe/app/src/main/java/com/example/moneyme/HomeActivity.java
+++ MoneyMe/app/src/main/java/com/example/moneyme/HomeActivity.java
@@ -1,18 +1,24 @@
 package com.example.moneyme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class HomeActivity extends AppCompatActivity {
     Button calculate;
     EditText income;
     DBHelper db;
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,37 @@ public class HomeActivity extends AppCompatActivity {
                     db.insertIncome(username, password, inco);
                     startActivity(intent2);
                 }
+            }
+        });
+
+
+        // Navigation Bar
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setSelectedItemId(R.id.home);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.person:
+                        startActivity(new Intent(getApplicationContext(), LogIn.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.money:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.progress:
+                        startActivity(new Intent(getApplicationContext(), Progress.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.info:
+                        startActivity(new Intent(getApplicationContext(), About.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        return true;
+                }
+                return false;
             }
         });
     }
