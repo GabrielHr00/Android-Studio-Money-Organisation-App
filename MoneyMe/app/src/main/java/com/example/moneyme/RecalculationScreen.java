58daf40d0b2b33ce9fbcab657f5244638db01dd8
gabrielh00 <a11914741@univie.ac.at>
2021-09-01 15:55:46 +0200

    Bug fixed with percentage change (info menu)

--- MoneyMe/app/src/main/java/com/example/moneyme/RecalculationScreen.java
+++ MoneyMe/app/src/main/java/com/example/moneyme/RecalculationScreen.java
@@ -1,17 +1,23 @@
 package com.example.moneyme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class RecalculationScreen extends AppCompatActivity {
 
     EditText username, savings, expences, free;
     Button change;
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +44,40 @@ public class RecalculationScreen extends AppCompatActivity {
                     String fre = free.getText().toString();
 
                     db.changePercentage(user, save, exp, fre);
-                    Intent intent = new Intent(getApplicationContext(), NewOverview.class);
+                    Intent intent = new Intent(getApplicationContext(), Overview.class);
                     intent.putExtra("user", user);
                     startActivity(intent);
                 }
+            }
+        });
+
+        // Navigation Bar
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setSelectedItemId(R.id.money);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.vote:
+                        startActivity(new Intent(getApplicationContext(), Vote.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.money:
+                        startActivity(new Intent(getApplicationContext(), MoneyScreen.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.progress:
+                        startActivity(new Intent(getApplicationContext(), Progress.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.info:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
             }
         });
     }
